c08e1a26fc7073a57e5ac90427645d68fc30f1c7
Tim Erickson <tim@in2words.org>
2016-06-26 21:09:41 -0700

    Add DictionaryXmlnsAttribute Add readString Add initial pass at readMultiByteInt31 (single byte vals only) Name records for debugging

--- nbfx/decoder.go
+++ nbfx/decoder.go
@@ -35,6 +35,7 @@ func (d *decoder) Decode(bin []byte) (string, error) {
 		if record == nil {
 			return "", errors.New(fmt.Sprintf("Unknown Record ID %#X", b))
 		}
+		//fmt.Println("Record:", record.name())
 		bytes, err := record.read(reader)
 		if err != nil {
 			return "", err
@@ -47,17 +48,25 @@ func (d *decoder) Decode(bin []byte) (string, error) {
 
 type record interface {
 	read(reader *bytes.Reader) ([]byte, error)
+	name() string
 }
 
 func getRecord(codec *codec, b byte) record {
 	if b == 0x56 {
 		return &prefixDictionaryElementS{codec}
+	} else if b == 0x0B {
+		return &dictionaryXmlnsAttribute{codec}
 	}
 	return nil
 }
 
+//(0x56)
 type prefixDictionaryElementS struct {
 	codec *codec
+}
+
+func (r *prefixDictionaryElementS) name() string {
+	return "PrefixDictionaryElementS (0x56)"
 }
 
 func (r *prefixDictionaryElementS) read(reader *bytes.Reader) ([]byte, error) {
@@ -70,4 +79,52 @@ func (r *prefixDictionaryElementS) read(reader *bytes.Reader) ([]byte, error) {
 		return []byte(val), nil
 	}
 	return nil, errors.New(fmt.Sprint("Invalid DictionaryString str", key))
+}
+
+//(0x0B)
+type dictionaryXmlnsAttribute struct {
+	codec *codec
+}
+
+func (r *dictionaryXmlnsAttribute) name() string {
+	return "dictionaryXmlnsAttribute (0x0B)"
+}
+
+func (r *dictionaryXmlnsAttribute) read(reader *bytes.Reader) ([]byte, error) {
+	strBytes, err := readString(reader)
+	if err != nil {
+		return strBytes, err
+	}
+	strBytes = append(strBytes, []byte(":")[0])
+	_, err = reader.ReadByte()
+	if err != nil {
+		return strBytes, err
+	}
+	return strBytes, nil
+}
+
+func readMultiByteInt31(reader * bytes.Reader) (uint32, error) {
+	b, err := reader.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	return uint32(b), nil //TODO: Handle multibyte values!!!
+}
+
+func readString(reader *bytes.Reader) ([]byte, error) {
+	var len uint32
+	len, err := readMultiByteInt31(reader)
+	if err != nil {
+		return nil, err
+	}
+	strBytes := *new([]byte)
+	for i := uint32(0); i < len; {
+		b, err := reader.ReadByte()
+		if err != nil {
+			return strBytes, err
+		}
+		strBytes = append(strBytes, b)
+		i++
+	}
+	return strBytes, nil
 }
