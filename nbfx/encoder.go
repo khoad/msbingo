1100f739ab66259a83852f5eda7887550cdf4d1e
Avinash H, Khoa Nguyen and Tim Erickson <tim@in2words.org>
2016-06-27 13:01:33 -0700

    Begin encode implementation

--- nbfx/encoder.go
+++ nbfx/encoder.go
@@ -1,7 +1,10 @@
 package nbfx
 
 import (
+	"bytes"
 	"errors"
+	"encoding/xml"
+	"fmt"
 )
 
 type encoder struct {
@@ -22,6 +25,29 @@ func NewEncoderWithStrings(dictionaryStrings map[uint32]string) Encoder {
 	return encoder
 }
 
-func (e *encoder) Encode(xml string) ([]byte, error) {
+func (e *encoder) Encode(xmlString string) ([]byte, error) {
+	reader := bytes.NewReader([]byte(xmlString))
+	binBuffer := &bytes.Buffer{}
+	xmlDecoder := xml.NewDecoder(reader)
+	token, err := xmlDecoder.RawToken()
+	for err == nil {
+		record := getRecordFromToken(token)
+		if record == nil {
+			return binBuffer.Bytes(), errors.New(fmt.Sprintf("Unknown Token %s", token))
+		}
+	}
 	return []byte{}, errors.New("NotImplemented: nbfx.Encoder.Encode(string)")
+}
+
+func getRecordFromToken(token xml.Token) record {
+	switch token.(type) {
+		case xml.StartElement:
+			return getStartElementRecordFromToken(token)
+	}
+
+	return nil
+}
+
+func getStartElementRecordFromToken(token xml.Token) record {
+	return nil
 }
