1fbe2cd222602fb3fdb5629a9dc06c008cc8c8c2
Erickson, Tim <Tim.M.Erickson@nordstrom.com>
2016-07-09 13:37:06 -0700

    Remove invalid test (is only valid in nbfs where dictionary is present)

--- nbfx/encoder_test.go
+++ nbfx/encoder_test.go
@@ -3,28 +3,8 @@ package nbfx
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"testing"
 )
-
-func TestEncodeExample1(t *testing.T) {
-	encoder := NewEncoder()
-	path := "../examples/1"
-	xmlBin, err := ioutil.ReadFile(path + ".xml")
-	if failOn(err, "unable to open "+path+".xml", t) {
-		return
-	}
-	expected, err := ioutil.ReadFile(path + ".bin")
-	if failOn(err, "unable to open "+path+".bin", t) {
-		return
-	}
-	actual, err := encoder.Encode(string(xmlBin))
-	if err != nil {
-		t.Error(fmt.Sprint(err.Error() + " : Got ", actual))
-		return
-	}
-	assertBinEqual(t, actual, expected)
-}
 
 func TestEncodePrefixDictionaryElementB(t *testing.T) {
 	xml := "<b:Foo>"
