ae0583981ccc674958828f6e0d14d0cfcfc2ff61
Khoa Nguyen <khoad@users.noreply.github.com>
2016-08-22 21:06:46 -0700

    Update nbfs codec.go constructors with comments

--- nbfs/codec.go
+++ nbfs/codec.go
@@ -2,10 +2,12 @@ package nbfs
 
 import "github.com/khoad/msbingo/nbfx"
 
+// NewDecoder creates a new NBFS Decoder
 func NewDecoder() nbfx.Decoder {
 	return nbfx.NewDecoderWithStrings(nbfsDictionary)
 }
 
+// NewEncoder creates a new NBFS Encoder
 func NewEncoder() nbfx.Encoder {
 	return nbfx.NewEncoderWithStrings(nbfsDictionary)
 }
